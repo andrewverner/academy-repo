71e1bb18a1cd84c5da83c066844e902f8dd0ac3b
Denis Khodakovskiy <denis.khodakovskiy@gmail.com>
2021-05-17 19:51:08 +0300

    Init

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 
-use Taskforce\Domain\Task;
-
 require_once 'vendor/autoload.php';
 
-$newTask = new Task(1, 3, 'new');
-
-
-print('<pre>');
-
-var_dump($newTask);
-
-print('</pre>');
+phpinfo();
